080d74e543f0094a10a88d3c5203257feb9c447a
iwymega <iwymega@gmail.com>
2026-01-18 09:58:28 +0800

    feat: update AppRouter to replace DashboardPage with EducationalVideoPage

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,7 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router";
 import LoginPage from "../auth/pages/LoginPage";
 import ForbiddenPage from "../auth/pages/ForbiddenPage";
 import RequireAuth from "../auth/middleware/RequireAuth";
-import DashboardPage from "../features/dashboard/pages/DashboardPage";
+// import DashboardPage from "../features/dashboard/pages/DashboardPage";
 import GuestOnly from "@/auth/middleware/GuestOnly";
 import UserManagementPage from "@/features/user-management/pages/UserManagementPage";
 import ProfilePage from "@/features/profile/pages/ProfilePage";
@@ -47,7 +47,7 @@ type AppRoute = ProtectedRoute | GuestOnlyRoute | PublicRoute;
 export const ROUTES: Record<string, AppRoute> = {
     DASHBOARD: {
         path: "/",
-        element: <DashboardPage />,
+        element: <EducationalVideoPage />,
         // protected: true,
         // roles: ["superadmin"],
         // permissions: [],
